d8e8b2273150897bb50a2e7a686330457bcc07db
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2015-01-30 06:28:22 +0700

    Remove unneeded do and use guard

--- Build.hs
+++ Build.hs
@@ -7,9 +7,9 @@ import Data.List
 import Control.Monad
 
 resolve :: FilePath -> FilePath -> FilePath
-resolve f _ | "_root" `isPrefixOf` f = '/' : dropDirectory1 f
-resolve ('_':f) home = home </> '.' : f
-resolve f _ = error $ "resolve: file path not supported (" ++ f ++ ")"
+resolve f home | "_root" `isPrefixOf` f = '/' : dropDirectory1 f
+               | ('_':g) <- f           = home </> '.' : g
+               | otherwise              = error $ "resolve: file path not supported (" ++ f ++ ")"
 
 cmdPairDo :: String -> (([FilePath] -> Action ()) -> FilePath -> FilePath -> Action ()) -> Action ()
 cmdPairDo act fn = do
@@ -19,8 +19,7 @@ cmdPairDo act fn = do
     forM_ argPairs $ \(src, tgt) -> fn (cmd act) src tgt
 
 main :: IO ()
-main = do
-    shakeArgs shakeOptions{shakeFiles=".shake/"} $ do
-        phony "pull" $ cmdPairDo "cp" (\cp s t -> cp [t, s])
-        phony "push" $ cmdPairDo "cp" (\cp s t -> cp [s, t])
-        phony "diff" $ cmdPairDo "diff -u" (\diff s t -> diff [t, s])
+main = shakeArgs shakeOptions{shakeFiles=".shake/"} $ do
+    phony "pull" $ cmdPairDo "cp" (\cp s t -> cp [t, s])
+    phony "push" $ cmdPairDo "cp" (\cp s t -> cp [s, t])
+    phony "diff" $ cmdPairDo "diff -u" (\diff s t -> diff [t, s])
